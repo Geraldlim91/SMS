0ac1a4954e0fcc2477b257b261687a75de093d39
Geraldlim91 <gerald_limjj@yahoo.com.sg>
2018-03-18 04:52:24 +0800

    Change config.py

---
+++ src/config.py
@@ -0,0 +1,17 @@
+class DB_CREDENTIALS :
+    DATABASE_ENGINE     = 'django.db.backends.postgresql_psycopg2'
+    DATABASE_NAME       = 'sms'
+    DATABASE_USER       = 'sms'
+    DATABASE_PASSWORD   = 'helloworld'
+    DATABASE_HOST       = '127.0.0.1'
+    DATABASE_PORT       = '5432'
+
+class MONGO_CREDENTIALS:
+    DATABASE_NAME       = 'sms'
+    # DATABASE_USER       = 'stamp'
+    # DATABASE_PASSWORD   = 'p@ssw0rd'
+
+
+
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
