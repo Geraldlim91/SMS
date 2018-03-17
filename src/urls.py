9a91cecec9c02168e92a3010f4c356160dfd32fb
Luke <37287282+lsw94@users.noreply.github.com>
2018-03-18 02:19:41 +0800

    Update URL for main

--- src/urls.py
+++ src/urls.py
@@ -3,5 +3,9 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^main/', include('src.login.urls'), ),
     url(r'^main/', include('src.register.urls')),
+<<<<<<< HEAD
     url(r'^main/', include('src.checkupreminder.urls'))
+=======
+    url(r'^main/', include('src.patient.urls')),
+>>>>>>> 5a4852bbe0fe58145c22fd72a0cec12a385ab606
 ]
