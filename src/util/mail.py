b575023311303566902c090e2d8941fa5f3237d9
Geraldlim91 <gerald_limjj@yahoo.com.sg>
2018-03-18 14:28:30 +0800

    Updated Mail

--- src/util/mail.py
+++ src/util/mail.py
@@ -1,6 +1,7 @@
 from smtplib import *
 import threading
 import traceback
+import subprocess
 
 from django.core.mail import send_mail
 
@@ -12,8 +13,8 @@ def SendMail(a_recipient, a_subject, a_message):
     sender = "Singapore Government Hospital <%s>" % ServerMail
 
     msg = "Dear Ms/Mr, \n\n " + a_message + "\n\nRegards, \nSingapore Government Hospital \n\n--------------------------------------------------------------------------- \nThis is a computer generated email. No reply is required."
-
     for eachEmail in recipient:
+
         message = """\
 From:  %s
 To: %s
@@ -22,21 +23,18 @@ Subject: %s
 %s
 """ % (sender, eachEmail, a_subject, msg)
 
-    try:
-        s = SMTP('smtp.gmail.com',587)
-        s.ehlo()
-        s.starttls()
-        s.ehlo()
-
-        s.login(ServerMail, ServerMailPassword)
-        s.sendmail(ServerMail, eachEmail, message)
-        s.quit()
-    except SMTPException:
-        print "Error: unable to send email"
+        try:
+            s = SMTP('smtp.gmail.com', 587)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(ServerMail, ServerMailPassword)
+            s.sendmail(ServerMail, eachEmail, message)
+            print eachEmail
+            s.quit()
+        except SMTPException:
+            print "Error: unable to send email"
 
 def uniq(alist):    # Fastest order preserving
     set = {}
     return [set.setdefault(e,e) for e in alist if e not in set]
-
-
-SendMail(['test12341@mailinator.com',], 'test', 'testing123')
