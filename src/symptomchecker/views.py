1f74b967553f40609639b7ec28733429a537e0f2
Luke <37287282+lsw94@users.noreply.github.com>
2018-03-18 22:36:04 +0800

    symptomcheck update

--- src/symptomchecker/views.py
+++ src/symptomchecker/views.py
@@ -6,9 +6,34 @@ from django.shortcuts import render
 
 
 # Create your views here.
-with open('../util/json/symptoms.json') as json_data:
-    data = json.load(json_data)
-    st = "Anxiety"
-    d = data[1][st]
-    print d
+def symptomcheck(symptoms):
+    diagnoses = []
+    with open('../util/json/symptoms.json') as json_data:
+        data = json.load(json_data)
+        for d in data:
+            for s in symptoms:
+                if (d.has_key(s)):
+                    diagnoses.append(d)
+
+    json_data.close()
+    return diagnoses
+                    # for i in d[s]:
+                    #     diagnoses.append(i)
+
+
+
+
+
+        #for d in data:
+            #for s symptoms:
+
+
+        #for key,value in data.items():
+            #print key
+            # for s in symptoms:
+            #     if s == value[s]:
+            #         diagnoses = value[s]
+            #         print diagnoses
+
+        #return diagnoses
 
